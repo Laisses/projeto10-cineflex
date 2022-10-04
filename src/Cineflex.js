cdca8197327409fbbbf381b32c3cddd86a8e4eae
Laisses <laisse@gmail.com>
2022-10-04 15:52:09 -0300

    add styled components to Header.js

--- src/Cineflex.js
+++ src/Cineflex.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { Header } from "./Header";
 import { Movies } from "./Movies";
@@ -7,13 +8,17 @@ import { Order } from "./Order";
 
 export const Cineflex = () => {
     return (
-        <>
+        <Container>
             <GlobalStyle />
             <Header />
             <Movies />
             <Session />
             <Seats />
             <Order />
-        </>        
+        </Container>        
     );
 }
+
+const Container = styled.div`
+    font-family: 'Roboto', sans-serif;
+`;
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
 export const Header = () => {
     return (
-        <div>
+        <Title>
              <h1>Cineflex</h1>   
-        </div>
+        </Title>
     );
 };
+
+const Title = styled.div`
+    width: 100%;
+    height: 67px;
+    background-color: #c3cfd9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h1 {
+        color: #e8833a;
+        font-size: 34px;
+        text-transform: uppercase;
+    }
+`;
